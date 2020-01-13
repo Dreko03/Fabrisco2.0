2ad4f9852717198162801409598c0034f178bf06
DrekoVR <Loic.pauleau@gmail.com>
2020-01-13 15:44:14 +0100

    Trucs commit

---
+++ Assets/_Materials/SC_Tableau.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Tableau : MonoBehaviour
+{
+    public Material tableau;
+    public Texture[] texture;
+    int init = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        init = 0;
+        InvokeRepeating("PaintingDisplay", 1, 0.05f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for (int i = 0; i < texture.Length; i++)
+        //{
+        //    tableau.mainTexture = texture[i];
+        //}
+    }
+
+    public void PaintingDisplay()
+    {
+        if (init == texture.Length)
+        {
+            init = 0;
+        }
+
+        tableau.mainTexture = texture[init];
+        init++;
+    }
+}
