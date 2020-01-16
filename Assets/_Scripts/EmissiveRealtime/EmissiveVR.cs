b1ebea9e08390189245b6869f3f55db5d43140ca
Loïc Pauleau <loic.pauleau@gmail.com>
2020-01-16 17:24:07 +0100

    light data scene Reflection

---
+++ Assets/_Scripts/EmissiveRealtime/EmissiveVR.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmissiveVR : MonoBehaviour
+{
+    public MeshRenderer _renderer;
+
+    void Start()
+    {
+        _renderer.GetComponent<MeshRenderer>();
+    }
+
+    
+    void Update()
+    {
+        float intensity = 5.0f;
+        Color final = Color.white * Mathf.LinearToGammaSpace(intensity);
+        _renderer.material.SetColor("_EmissionColor", final);
+        DynamicGI.SetEmissive(_renderer, final);
+
+    }
+}
